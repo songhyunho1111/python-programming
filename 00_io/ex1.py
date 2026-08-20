c6e13f55c9d599e6816a0b3b9ee5b7318a383f6b
songhyunho1111 <songhh06101@gmail.com>
2026-08-20 15:20:10 +0900

    Add input/output + type casting via map

---
+++ 00_io/ex1.py
@@ -0,0 +1,19 @@
+a = input()
+print(a)
+print(type(a))
+print(a, type(a), sep=",")
+
+a = int(input())
+
+print(a, type(a))
+
+a = int(input())
+b = int(input())
+print(a, b)
+
+a, b = input().split()
+a, b = int(a), int(b)
+
+a,b=map(int, input().split())
+
+print(a, type(a))
