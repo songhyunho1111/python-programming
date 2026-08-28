a55d381da7f29d11bc50f3352c9690e818dbb493
songhyunho1111 <songhh06101@gmail.com>
2026-08-28 20:42:10 +0900

    ex6. string

--- 01_datatype/ex5.py
+++ 01_datatype/ex5.py
@@ -3,13 +3,13 @@
 a = "Hello, Python"
 
 # # dir() 함수: 객체가 가지고 있는 속성과 메소드 목록을 반환하는 파이썬 내장 함수
-# print()                       # str 자료형이 제공하는 속성과 메소드 목록
-# print()                      # int 자료형이 제공하는 속성과 메소드 목록
-# print()                      # list 자료형이 제공하는 속성과 메소드 목록
+# print(dir(a))                       # str 자료형이 제공하는 속성과 메소드 목록
+# print(dir(1))                      # int 자료형이 제공하는 속성과 메소드 목록
+# print(dir(list))                      # list 자료형이 제공하는 속성과 메소드 목록
 
 # # len() 함수: 객체의 길이를 반환하는 파이썬 내장 함수
 print(len(a))                       # 문자열의 길이
-print(len(a))               # 리스트의 길이(요소의 개수)
+print(len([1, 2, 3]))               # 리스트의 길이(요소의 개수)
 
 # # 대소문자를 변환해주는 문자열 메소드
 print(a.upper())                    # 대문자로 변환
@@ -18,60 +18,61 @@ print(a.capitalize())               # 문자열의 첫 글자만 대문자로 �
 # print(a.)                    # 각 단어의 첫 글자만 대문자로 변환
 
 # # 문자열의 공백문자 or 특정문자를 제거하는 문자열 메소드
-# a = "\t  python  \n"
-# print()
-# print()       # 왼쪽 공백문자 제거
-# print()       # 오른쪽 공백문자 제거
-# print()        # 양쪽 공백문자 제거
+a = "\t  python  \n"
+print("[" + a + "]")
+print("[" + a.lstrip() + "]")       # 왼쪽 공백문자 제거
+print("[" + a.rstrip() + "]")       # 오른쪽 공백문자 제거
+print("[" + a.strip() + "]")        # 양쪽 공백문자 제거
 
-# a = "***python***"
-# print()                # 왼쪽 특정문자 제거
-# print()                # 오른쪽 특정문자 제거
-# print()                 # 양쪽 특정문자 제거
+a = "***python***"
+print(a.lstrip("*"))                # 왼쪽 특정문자 제거
+print(a.rstrip("*"))                # 오른쪽 특정문자 제거
+print(a.strip("*"))                 # 양쪽 특정문자 제거
 
 
-# s = "Python is fun. I love Python."
+s = "Python is fun. I love Python."
 
-# # 부분 문자열이 처음 등장하는 위치(인덱스)를 알려주는 문자열 메소드
-# print()             # 인덱스 반환
-# print()            # 인덱스 반환
+# 부분 문자열이 처음 등장하는 위치(인덱스)를 알려주는 문자열 메소드
+print(s.find("Python"))            # 인덱스 반환
+print(s.index("Python"))             # 인덱스 반환
 
-# print()               # 인덱스 반환 ( 없으면 )
-# print()            # 인덱스 반환 ( 없으면 )
+print(s.find("Java"))               # 인덱스 반환 ( 없으면 )
+# print(s.index("Crong"))            # 인덱스 반환 ( 없으면 )
 
 # # 부분 문자열이 몇 번 나오는지 알려주는 문자열 메소드
-# print()                 # 등장 횟수
+print(s.count("o"))                 # 등장 횟수
 
-# # 문자열 포함여부를 알려주는 연산자
-# print()                # 포함(True)
-# print()                  # 미포함(False)
+# 문자열 포함여부를 알려주는 연산자
+print("Python" in s)                # 포함(True)
+print("Java"  not in s)                  # 미포함(False)
 
 # # 특정 Prefix로 시작하고 있는지 알려주는 문자열 메소드
-# print()       # True
+print(s.startswith("Python"))       # True
 
 # # 특정 Suffix로 끝나고 있는지 알려주는 문자열 메소드
-# print()         # False
+print(s.endswith("."))         # False
 
 # # 이전 문자열을 새로운 문자열로 치환하는 문자열 메소드
 # # 문제) replace는 원본 문자열을 바꿀까요? 새로운 문자열을 만들까요?
-# print()     # 전체 치환
-# print()  # 1개만 치환
+print(s.replace("Python", "C"))     # 전체 치환
+print()  # 1개만 치환
 
 # # 판별 문자열 메소드 (isXXX())
-# print("123")              # 숫자이면 True
-# print("abc")              # 알파벳이면 True
-# print("abc123")           # 알파벳 + 숫자이면 True
-# print(" \t \n")           # 공백문자이면 True
-# print("hello")            # 소문자이면 True
-# print("HELLO")            # 대문자이면 True
+print("123".isdigit())              # 숫자이면 True
+# print("123".isnumeric())              # 숫자이면 True | 한자, 로마자 가능 !!
+print("abc".isalpha())              # 알파벳이면 True
+print("abc123".isalnum())           # 알파벳 + 숫자이면 True
+print(" \t \n".isspace())           # 공백문자이면 True
+print("hello".islower())            # 소문자이면 True
+print("HELLO".isupper())            # 대문자이면 True
 
 # # 구분자를 기준으로 문자열을 분리하는 문자열 메소드
-# a = "apple,banana,kiwi"
-# fruits = None                # ","를 기준으로 분리 (기본값 공백)
-# print(fruits)                       # 리스트가 만들어짐
+a = "apple,banana,kiwi"
+fruits = a.split(",")                # ","를 기준으로 분리 (기본값 공백)
+print(fruits)                       # 리스트가 만들어짐
 
 # # Iterable(반복가능) 객체안의 문자열을 결합하는 문자열 메소드
-# print()             # ","로 연결하여 결합
+print(",".join(fruits))             # ","로 연결하여 결합
 
 
 # =========================================================
