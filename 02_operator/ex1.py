894fc762fda6a8e37436578235e0e58e68f82cd9
songhyunho1111 <songhh06101@gmail.com>
2026-08-31 11:30:17 +0900

    02_operator

---
+++ 02_operator/ex1.py
@@ -0,0 +1,24 @@
+# 연산자
+
+# 산술 연산자
+a = 10
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)  # 나머지
+print(a // b)  # 몫
+print(a**b)  # 거듭 제곱
+
+# 복합 대입 연산자
+a += 4
+print(a)
+
+a -= 2
+print(a)
+
+# 증감 연산자
+# b = a++
+a += 1
